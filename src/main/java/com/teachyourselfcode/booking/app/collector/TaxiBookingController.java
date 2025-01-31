2ef488f54349eb8759f97b9af07913be47934c70
karin <karin@DESKTOP-JFTOVL2>
2025-01-31 18:55:10 +0100

    9. How to create UPDATE REQUEST Using SPRING BOOT !!!

--- src/main/java/com/teachyourselfcode/booking/app/collector/TaxiBookingController.java
+++ src/main/java/com/teachyourselfcode/booking/app/collector/TaxiBookingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,10 @@ public class TaxiBookingController {
 	@DeleteMapping(value="/deletebooking/{bookingId}")
 	public void deleteBooking(@PathVariable("bookingId") Integer bookingId) {
 	    taxiBookingService.deleteBooking(bookingId);
+	}
+	
+	@PutMapping(value="/updatebooking")
+	public Taxi updateBooking(@RequestBody Taxi taxi) {
+		return taxiBookingService.createBooking(taxi);
 	}
 }
